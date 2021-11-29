e51cd7e7f1c3f335bd7ec802470a1f8fdab85793
AntonyToron <antony.toron@gmail.com>
2021-11-28 22:25:22 -0500

    Added a new route and also rate limiting on it

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 # Author: Antony Toron
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["200 per day", "50 per hour"]
+)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/sentiment", methods=["POST"])
+@limiter.limit("100 per day")
+def sentiment():
+    target_entity = request.form["entity"]
+    return render_template("sentiment.html")
